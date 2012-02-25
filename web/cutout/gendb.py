a15235c8288f1e33469da4edbf260279a322def2
Randal Burns <randal@cs.jhu.edu>
2012-02-25 12:11:42 -0500

    Change kasthuri to res0 base

--- web/cutout/gendb.py
+++ web/cutout/gendb.py
@@ -26,11 +26,8 @@ def main():
 
   #  Specify the database to ingest
   dbcfg = dbconfigkasthuri11.dbConfigKasthuri11()
-<<<<<<< HEAD
 #  dbcfg = dbconfigbock11.dbConfigBock11()
 #  dbcfg = dbconfighayworth5nm.dbConfigHayworth5nm()
-=======
->>>>>>> adeafdd5ebe9b00543b9a410e2c4156bb5e15c2f
 
   cdb = cubedb.CubeDB ( dbcfg )
 
