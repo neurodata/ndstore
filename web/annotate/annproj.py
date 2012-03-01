5c874393bced496c6b6c528bd78e18b271c35e35
Randal Burns <randal@cs.jhu.edu>
2012-02-29 19:00:54 -0500

    Added projects

---
+++ web/annotate/annproj.py
@@ -0,0 +1,123 @@
+import empaths
+import MySQLdb
+
+import annpriv
+
+#
+#  AnnotateCube: manipulate the in-memory data representation of the 3-d cube of data
+#    that contains annotations.  
+#
+
+
+#
+# Annotation project not found
+#
+class AnnoProjException (Exception):
+  """Could not load and annotation project"""
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+
+
+class AnnotateProject:
+  """Project specific annotation data"""
+
+  # Constructor 
+  def __init__(self, dbname, dataset, resolution ):
+    """Initialize the Annotation Project"""
+    
+    self._dbname = dbname
+    self._dataset = dataset
+    self._resolution = resolution
+
+    # Could add these to dbconfig.  Probably remove res as tablebase instead
+    self._ids_tbl = "ids"
+    self._except_tbl = "exceptions"
+    self._ann_tbl = "annotations" 
+
+
+  # Accessors
+  def getDBName ( self ):
+    return self._dbname
+  def getDataset ( self ):
+    return self._dataset
+  def getResolution ( self ):
+    return self._resolution
+  def getIDsTbl ( self ):
+    return self._ids_tbl
+  def getEntitiesTbl ( self ):
+    return self._entities_tbl
+  def getExceptionsTbl ( self ):
+    return self._exceptionss_tbl
+  def getAnnotationsTbl( self ):
+    return self._ann_tbl
+
+
+class AnnotateProjectsDB:
+  """Database for the annotation projects"""
+
+  def __init__(self):
+    """Create the database connection"""
+
+    # Connection info in dbconfig
+    self.conn = MySQLdb.connect (host = annpriv.dbhost,
+                          user = annpriv.dbuser,
+                          passwd = annpriv.dbpasswd,
+                          db = annpriv.db )
+
+  #
+  # Load the annotation databse information based on the token
+  #
+  def getAnnoProj ( self, token ):
+    """Load the annotation database information based on the token"""
+
+
+    # Lookup the information for the database project based on the token
+    sql = "SELECT * from %s where token = %s" % (annpriv.table, token)
+
+    # RBTODO database query
+    try:
+      cursor = self.conn.cursor()
+      cursor.execute ( sql )
+    except MySQLdb.Error, e:
+      print "Could not query annotations projects database"
+      raise AnnoProjException ( "Annotation Project Database error" )
+
+    # get the project information 
+    row = cursor.fetchone()
+
+    # if the project is not found.  error
+    if ( row == None ):
+      print "No project found"
+      raise AnnoProjException ( "Project token not found" )
+
+    [token, openid, project, dataset, resolution] = row
+
+    return AnnotateProject ( project , dataset, resolution )
+
+
+  #
+  # Load the annotation databse information based on the token
+  #
+  def newAnnoProj ( self, token, openid, project, dataset, resolution ):
+    """Create a new annotation project"""
+
+    # RBTODO some checking that the dataset and resolution are legal???
+
+    sql = "INSERT INTO {0} VALUES ( {1}, \'{2}\', \'{3}\', \'{4}\', {5} )".format (\
+        annpriv.table, token, openid, project, dataset, resolution )
+
+    print annpriv.table, sql
+
+    # RBTODO database query
+    try:
+      cursor = self.conn.cursor()
+      cursor.execute ( sql )
+    except MySQLdb.Error, e:
+      print "Failed to create new project", e
+      raise AnnoProjException ( "Failed to create new project" )
+
+    self.conn.commit()
+    # RBTODO create the tables
+
