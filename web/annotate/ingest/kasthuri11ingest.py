fed420465266d04adde942fea9955881881aeec8
Randal Burns <randal@cs.jhu.edu>
2012-03-06 18:03:30 -0500

    Modify for first ingest try

--- web/annotate/ingest/kasthuri11ingest.py
+++ web/annotate/ingest/kasthuri11ingest.py
@@ -27,9 +27,11 @@ _ytiles = 2
 _xtilesz = 8192
 _ytilesz = 8192
 _startslice = 0000
-_endslice = 1824  
+_endslice = 1849  
 _prefix = 'fullresseg22312_s'
-_batchsz = 2
+
+# Was going to do by batches, but the PNG is so inefficient it doesn't matter
+#_batchsz = 16
 
 
 
@@ -76,8 +78,7 @@ def main():
   voxellists = collections.defaultdict(list)
 
   # Get a list of the files in the directories
-#  for sl in range (_startslice,_endslice+1):
-  for sl in range (_startslice,2): #RBTODO
+  for sl in range (_startslice,_endslice+1):
     for y in range ( _ytiles ):
       for x in range ( _xtiles ):
         filenm = result.path + '/' + _prefix + '{:0>4}'.format(sl) + '_Y' + str(y) + '_X' + str(x) + '.png'
@@ -92,50 +93,15 @@ def main():
         while not it.finished:
           if ( it[0] != 0 ):
             voxellists[ str( it[0] ) ].append ( [ it.multi_index[1] + y*_ytilesz, it.multi_index[0] + x*_xtilesz, sl+1 ] )
-            print "Found nonzero", it[0], " adding at ", it.multi_index[1] + y*_ytilesz, it.multi_index[0] + x*_xtilesz, sl+1
           it.iternext()
 
-#        it = np.nditer ( imgdata, flags=['multi_index'], op_flags=['readonly'] )
-#        while not it.finished:
-#          val = (it[0] << 16)
-#          it.iternext()
-#          val += (it[0] << 8)
-#          it.iternext()
-#          val += it[0]
-#          it.iternext()
-#          if val:
-            # RBTODO Need to offset by x and y tiles
-#            voxellists[ str( val ) ].append ( [ it.multi_index[1] + y*_ytilesz, it.multi_index[0] + x*_xtilesz, sl+1 ] )
-#            print "Found nonzero", val, " adding at ", it.multi_index[1] + y*_ytilesz, it.multi_index[0] + x*_xtilesz, sl+1
 
-#  Still need to do this by batch size.
-
-        # ingest after the first tile
+    # Send the annotation lists to the database
     for key, voxlist in voxellists.iteritems():
 
-            url = 'http://0.0.0.0:8080/annotate/%s/npadd/%s/' % (token,key)
-
-            print url
-
-            fileobj = cStringIO.StringIO ()
-            np.save ( fileobj, voxlist )
-
-            # Build the post request
-            req = urllib2.Request(url, fileobj.getvalue())
-            response = urllib2.urlopen(req)
-            the_page = response.read()
-
-
-  # Send the annotation lists to the database
-  fileobj = cStringIO.StringIO ()
-
-  for key, voxlist in voxellists.iteritems():
-
       url = 'http://0.0.0.0:8080/annotate/%s/npadd/%s/' % (token,key)
-
-      print key, voxlist
       print url
-
+      fileobj = cStringIO.StringIO ()
       np.save ( fileobj, voxlist )
 
       # Build the post request
@@ -143,9 +109,9 @@ def main():
       response = urllib2.urlopen(req)
       the_page = response.read()
 
-# For now do them all as a batch
+    # Clear the voxel list -- old one gets garbage collected
+    voxlist = {}
 
-  
 
 if __name__ == "__main__":
   main()
