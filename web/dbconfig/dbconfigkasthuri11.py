7255fb3108c020e49080ad9279a1c09bed2b76d7
Randal Burns <randal@cs.jhu.edu>
2012-05-23 14:50:07 -0400

    Config after ingest

--- web/dbconfig/dbconfigkasthuri11.py
+++ web/dbconfig/dbconfigkasthuri11.py
@@ -23,8 +23,7 @@ class dbConfigKasthuri11 ( dbconfig.dbConfig ):
   tilesz = [ 256,256 ]
 
   #resolution information -- lowest resolution and list of resolution
-#  resolutions = [ 7, 6, 5, 4, 3, 2, 1, 0 ]
-  resolutions = [ 4 ]
+  resolutions = [ 7, 6, 5, 4, 3, 2, 1, 0 ]
 
   imagesz = { 7: [ 256, 256 ],\
               6: [ 2*256, 2*256 ],\
