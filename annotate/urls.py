95ea52ecc287800b50e8a8a97fdf4335c2660ab0
Randal Burns <randal@cs.jhu.edu>
2012-06-05 11:14:26 -0400

    Fix URLs.

--- annotate/urls.py
+++ annotate/urls.py
@@ -5,14 +5,14 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('annotate.views',
-  # annotations get and put: must be a number
-  url(r'(?P<webargs>^\w+/[\d+/]?[\w,/]*)$', 'annotation'),
   # test 
   url(r'^$', 'index'),
   # get services
   url(r'(?P<webargs>^\w+/(xy|xz|yz|hdf5|npz)/[\w,/]+)$', 'annoget'),
   # the post services
   url(r'(?P<webargs>^\w+/(npvoxels|npdense)/[\w,/]+)$', 'annopost'),
+  # annotations get and put: must be a number
+  url(r'(?P<webargs>^\w+/[\d+/]?[\w,/]*)$', 'annotation'),
   url(r'^post/$', 'post_test'),
   url(r'^admin/', include(admin.site.urls)),
 )
