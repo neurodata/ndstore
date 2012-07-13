ec65201750fcd2fdc7e78a0549ddec48ebaf655a
Randal Burns <randal@cs.jhu.edu>
2012-07-13 12:01:59 -0400

    Change HDFS field from "cube" to "CUTOUT"

--- web/cutout/brainrest.py
+++ web/cutout/brainrest.py
@@ -70,7 +70,7 @@ def HDF5 ( webargs, dbcfg ):
   # Create an in-memory HDF5 file
   tmpfile = tempfile.NamedTemporaryFile ()
   fh5out = h5py.File ( tmpfile.name )
-  ds = fh5out.create_dataset ( "cube", tuple(cube.data.shape), np.uint8,\
+  ds = fh5out.create_dataset ( "CUTOUT", tuple(cube.data.shape), np.uint8,\
                                  compression='gzip', data=cube.data )
   fh5out.close()
   tmpfile.seek(0)
