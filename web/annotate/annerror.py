beb974a0e2fbd3ac85fb99c1e60889a8feb9af9b
Randal Burns <randal@cs.jhu.edu>
2012-07-18 13:55:31 -0400

    Adding annerror.

---
+++ web/annotate/annerror.py
@@ -0,0 +1,10 @@
+#
+# Annotation errors
+#
+class ANNError(Exception):
+  """General annotation error"""
+  def __init__(self, value):
+    self.value = value
+  def __str__(self):
+    return repr(self.value)
+
