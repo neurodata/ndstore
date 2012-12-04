df6a526107242f0cf36ec65f5cfbccbf8779ddcd
Randal Burns <randal@cs.jhu.edu>
2012-12-04 14:50:20 -0500

    Add paths.

---
+++ web/ingest/ssmith/empaths.py
@@ -0,0 +1,13 @@
+#
+# Code to load project paths
+#
+
+import os, sys
+
+EM_BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.." ))
+EM_UTIL_PATH = os.path.join(EM_BASE_PATH, "util" )
+EM_DBCONFIG_PATH = os.path.join(EM_BASE_PATH, "dbconfig" )
+EM_EMCA_PATH = os.path.join(EM_BASE_PATH, "emca" )
+
+sys.path += [ EM_UTIL_PATH, EM_DBCONFIG_PATH, EM_EMCA_PATH ]
+
