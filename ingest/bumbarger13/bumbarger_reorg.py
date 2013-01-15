33c65c7e63127d13885c907f6c5adb4862c41cf7
Randal Burns <randal@braingraph1dev.cs.jhu.edu>
2013-01-15 17:46:55 -0500

    Added multiple slice support.

--- ingest/bumbarger13/bumbarger_reorg.py
+++ ingest/bumbarger13/bumbarger_reorg.py
@@ -4,21 +4,29 @@ import shutil
 inputdir = '/data/EM/bumbarger13'
 outputdir = '/data/EM/bumbarger13.small'
 
-# resolutions
+#
+#  Customize ingest script to turn the output of TrackEM export 
+#  into OCP-naming-convention CATMAID stack.
+#
+
+#
+# The bumbarger data is somewhere in the middle of the field.  These parameters
+#  specify its location and properties of the resulting CATMAID stack
+#
+
+# Highest resolution: ignore 5,6,7
 highres = 4   # 0 to 4
 
-# Active slices at resolution 
+# Active tiles at resolution 4
 # x = 11-14
 # y = 10-13
 xrange = (11,15)
 yrange = (10,14)
-
-# geometry of the space at high resolution
 xtiles = xrange[1]-xrange[0]
 ytiles = yrange[1]-yrange[0]
 
-# need to loop on this later
-slices = [0]
+# Slices to ingest [first,last]
+slices = [0,1]
 
 # build the directories
 for res in range(highres+1):
@@ -29,28 +37,28 @@ for res in range(highres+1):
     os.mkdir ( dirname )
 
   #inner directory is slicenumber
-  for slice in slices:
+  for slice in range(slices[0],slices[1]+1):
     dirname = "%s/%s/%s" % ( outputdir, res, slice )
     if not os.path.exists ( dirname ):
       os.mkdir ( dirname )
 
 
 for res in range(highres,-1,-1):
-  for ytile in range(xtiles*2**(highres-res)):
-    for xtile in range(ytiles*2**(highres-res)):
+  for slice in range(slices[0],slices[1]+1):
+    for ytile in range(xtiles*2**(highres-res)):
+      for xtile in range(ytiles*2**(highres-res)):
 
-      xoffset = xrange[0]*2**(highres-res) 
-      yoffset = yrange[0]*2**(highres-res)
-
-      infile = "%s/%s/%s_%s_%s.png" % ( inputdir, slice, ytile+yoffset, xtile+xoffset, res )
-      outfile = "%s/%s/%s/%s_%s.png" % ( outputdir, res, slice, ytile, xtile )
-
-      try:
-        if not os.path.exists(outfile):
-          print "Copying %s to %s" % ( infile, outfile )
-          shutil.copy ( infile, outfile )
-        else: 
-          print "Found %s" % ( outfile )
-      except:
-        print "Missing file %s" % ( infile )
-
+        xoffset = xrange[0]*2**(highres-res) 
+        yoffset = yrange[0]*2**(highres-res)
+  
+        infile = "%s/%s/%s_%s_%s.png" % ( inputdir, slice, ytile+yoffset, xtile+xoffset, res )
+        outfile = "%s/%s/%s/%s_%s.png" % ( outputdir, res, slice, ytile, xtile )
+  
+        try:
+          if not os.path.exists(outfile):
+            print "Copying %s to %s" % ( infile, outfile )
+            shutil.copy ( infile, outfile )
+          else: 
+            print "Found %s" % ( outfile )
+        except:
+          print "Missing file %s" % ( infile )
