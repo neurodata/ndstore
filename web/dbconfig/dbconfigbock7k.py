c7a3cfd7c8a915d615209742129feeade557f2db
randalburns <randal@cs.jhu.edu>
2013-04-02 18:27:36 -0400

    Bock7k ingest.

--- web/dbconfig/dbconfigbock7k.py
+++ web/dbconfig/dbconfigbock7k.py
@@ -4,20 +4,20 @@
 
 import dbconfig
 
-class dbConfigWill ( dbconfig.dbConfig ):
+class dbConfigBock7k ( dbconfig.dbConfig ):
 
   # cubedim is a dictionary so it can vary 
   # size of the cube at resolution
   cubedim = { 0: [128, 128, 16] }
 
   #information about the image stack
-  slicerange = [0,1023]
+  slicerange = [0,61]
   tilesz = [ 256,256 ]
 
   #resolution information -- lowest resolution and list of resolution
   resolutions = [ 0 ]
 
-  imagesz = { 0: [ 65536, 65536 ] }
+  imagesz = { 0: [ 7198, 7352 ] }
 
   # Resize factor to eliminate distortion
   zscale = { 0: 1.0 }
