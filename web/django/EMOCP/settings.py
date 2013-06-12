0439ab030eed3025f0cc680867efc55252c97daa
Randal Burns <randal@cs.jhu.edu>
2013-06-12 11:49:38 -0400

    Prior to datasetdb merge

--- web/django/EMOCP/settings.py
+++ web/django/EMOCP/settings.py
@@ -120,7 +120,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/priya/EM-connectome/web/django/templates'
+    '/var/www/EM-connectome/web/django/templates'
 )
 
 INSTALLED_APPS = (
