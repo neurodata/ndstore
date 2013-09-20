77b4a3cbabaedaa58fed07ee64a32f114f21deb6
randalburns <randal@cs.jhu.edu>
2013-09-20 11:54:48 -0400

    Small cnage to image cuboid.

--- web/emca/emcadb.py
+++ web/emca/emcadb.py
@@ -1174,7 +1174,7 @@ class EMCADB:
   #
   # writeImageCuboid
   #
-  #  Write an image through the Web service.  Make ingest easier.
+  #  Write data that is not integral in cuboids
   #
   def writeImageCuboid ( self, corner, resolution, imgdata ):
     """Write an image through the Web service"""
