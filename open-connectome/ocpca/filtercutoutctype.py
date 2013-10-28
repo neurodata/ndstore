fdc5180e10476524a1074a1b3eec4dc468904575
Kunal Lillaney <klillan1@jhu.edu>
2013-10-28 13:45:07 -0400

    Adding the filter library and the filter files in ocpca

---
+++ open-connectome/ocpca/filtercutoutctype.py
@@ -0,0 +1,48 @@
+import ctypes as cp
+import numpy as np
+import numpy.ctypeslib as npct
+import os
+import empaths
+
+#
+# Filter Cutout using ctypes
+# Has 2 implentations of filter function
+# 1 - Naive Implementation 
+# 2 - OpenMP Implementation
+#
+
+# Getting the filter directory path
+filterdirpath = os.path.join(empaths.EM_EMCA_PATH,"filterlib")
+# Load the shared C library using numpy mechanism
+filterlib = npct.load_library("filterCutout",filterdirpath) 
+filterlibOMP = npct.load_library("filterCutoutOMP",filterdirpath)
+
+array_1d_uint32 = npct.ndpointer(dtype=np.uint32, ndim=1, flags='CONTIGUOUS')
+
+# defining the parameter types of the functions in C
+# FORMAT: <library_name>,<functiona_name>.argtypes = [ ctype.<argtype> , ctype.<arg    type> ....]
+filterlib.filterCutout.argtypes = [array_1d_uint32, cp.c_int, array_1d_uint32, cp.c_int]
+filterlibOMP.filterCutoutOMP.argtypes = [array_1d_uint32, cp.c_int, array_1d_uint32, cp.c_int]
+
+# setting the return type of the function in C
+# FORMAT: <library_name>.<function_name>.restype = [ ctype.<argtype> ]
+filterlib.filterCutout.restype = None
+filterlibOMP.filterCutoutOMP.restype = None
+
+# Uses OpenMP for filtercutout
+def filterCutoutCtypeOMP ( cutout, filterlist ):
+		"""Remove all annotations in a cutout that do not match the filterlist using OpenMP"""
+		# get a copy of the iterator as a 1-D array
+		flatcutout = cutout.flat.copy()
+		#Calling the C openmp funtion 
+		filterlibOMP.filterCutoutOMP(flatcutout,cp.c_int(len(flatcutout)),filterlist,cp.c_int(len(filterlist)))
+		return flatcutout.reshape(cutout.shape[0],cutout.shape[1],cutout.shape[2])
+
+# Uses naive implementation of C for filtercutout
+def filterCutoutCtype ( cutout, filterlist ):
+		"""Remove all annotations in a cutout that do not match the filterlist"""
+		# get a copy of the iterator as a 1-D array
+		flatcutout = cutout.flat.copy()
+		# Calling the C naive function
+		filterlib.filterCutout(flatcutout,cp.c_int(len(flatcutout)),filterlist,cp.c_int(len(filterlist)))
+		return flatcutout.reshape(cutout.shape[0],cutout.shape[1],cutout.shape[2])
