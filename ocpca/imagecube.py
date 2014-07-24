5b7a8aceacd0dfea17da4e7d35bbecbd5a991bb2
Kunal Lillaney <klillan1@jhu.edu>
2014-07-23 20:28:50 -0400

    Adding support for RGB32 and RGB64 image cubes.

--- ocpca/imagecube.py
+++ ocpca/imagecube.py
@@ -49,6 +49,7 @@ class ImageCube8(Cube):
   def xySlice ( self, fileobj ):
 
     zdim,ydim,xdim = self.data.shape
+    import pdb; pdb.set_trace()
     outimage = Image.frombuffer ( 'L', (xdim,ydim), self.data[0,:,:].flatten(), 'raw', 'L', 0, 1 ) 
     outimage.save ( fileobj, "PNG" )
   
@@ -126,6 +127,9 @@ class ImageCube16(Cube):
     zdim,ydim,xdim = self.data.shape
     outimage = Image.frombuffer ( 'I;16', (xdim,ydim), self.data[0,:,:].flatten(), 'raw', 'I;16', 0, 1) 
     outimage = outimage.point(lambda i:i*(1./256)).convert('L')
+    #self.data = np.clip ( self.data, 0, 565)
+    #self.data = (np.uint8)( self.data/(565.0/255) )
+    #outimage = Image.fromarray(self.data[0,:,:])
     outimage.save ( fileobj, "PNG" )
 
 
@@ -152,3 +156,162 @@ class ImageCube16(Cube):
 
 # end BrainCube
 
+### Added by Kunal ###
+
+#
+#  ImageCube32: manipulate the in-memory data representation of the 3-d cube 
+#    includes loading, export, read and write routines
+#
+#  This sub-class is for 32 bit data. The data is 4x8bit RGBA channels.
+#
+#  All the interfaces to this class are in x,y,z order
+#  Cube data goes in z,y,x order this is compatible with images and more efficient?
+#
+class ImageCube32(Cube):
+
+  # Constructor 
+  def __init__(self, cubesize=[64,64,64]):
+    """Create empty array of cubesize"""
+
+    # call the base class constructor
+    Cube.__init__(self,cubesize)
+    # note that this is self.cubesize (which is transposed) in Cube
+    self.data = np.zeros ( self.cubesize, dtype=np.uint32 )
+
+  # was the cube created from zeros?
+  def fromZeros ( self ):
+    """Determine if the cube was created from all zeros?"""
+    if self._newcube == True:
+      return True
+    else: 
+      return False
+
+  # create an all zeros cube
+  def zeros ( self ):
+    """Create a cube of all 0"""
+    self._newcube = True
+    self.data = np.zeros ( self.cubesize, dtype=np.uint32 )
+
+  def overwrite ( self, annodata ):
+    """Get's a dense voxel region and overwrites all non-zero values"""
+
+    vector_func = np.vectorize ( lambda a,b: b if b!=0 else a ) 
+    self.data = vector_func ( self.data, annodata ) 
+
+
+  #
+  # Create the specified slice (index) at filename
+  #
+  def xySlice ( self, fileobj ):
+
+    zdim,ydim,xdim = self.data.shape
+    outimage = Image.fromarray( self.data[0,:,:], "RGBA")
+    outimage.save ( fileobj, "PNG" )
+
+
+  #
+  # Create the specified slice (index) at filename
+  #
+  def xzSlice ( self, zscale, fileobj  ):
+
+    zdim,ydim,xdim = self.data.shape
+    outimage = Image.fromarray( self.data[:,0,:], "RGBA")
+    outimage.save ( fileobj, "PNG" )
+
+
+  #
+  # Create the specified slice (index) at filename
+  #
+  def yzSlice ( self, zscale, fileobj  ):
+
+    zdim,ydim,xdim = self.data.shape
+    outimage = Image.fromarray( self.data[:,:,0], "RGBA")
+    outimage.save ( fileobj, "PNG" )
+
+  #
+  # Convert the uint32 back into 4x8 bit channels
+  #
+  def RGBAChannel ( self ):
+
+    zdim, ydim, xdim = self.data.shape
+    newcube = np.zeros( (4, zdim, ydim, xdim), dtype=np.uint8 )
+    newcube[0,:,:,:] = np.bitwise_and(self.data, 0xff, dtype=np.uint8)
+    newcube[1,:,:,:] = np.uint8 ( np.right_shift( self.data, 8) & 0xff )
+    newcube[2,:,:,:] = np.uint8 ( np.right_shift( self.data, 16) & 0xff )
+    newcube[3,:,:,:] = np.uint8 ( np.right_shift (self.data, 24) )
+    self.data = newcube
+
+
+# end BrainCube
+
+#
+#  ImageCube64: manipulate the in-memory data representation of the 3-d cube 
+#    includes loading, export, read and write routines
+#
+#  This sub-class is for 64 bit data. The data is 4x8bit RGBA channels.
+#
+#  All the interfaces to this class are in x,y,z order
+#  Cube data goes in z,y,x order this is compatible with images and more efficient?
+#
+class ImageCube64(Cube):
+
+  # Constructor 
+  def __init__(self, cubesize=[64,64,64]):
+    """Create empty array of cubesize"""
+
+    # call the base class constructor
+    Cube.__init__(self,cubesize)
+    # note that this is self.cubesize (which is transposed) in Cube
+    self.data = np.zeros ( self.cubesize, dtype=np.uint64 )
+
+  # was the cube created from zeros?
+  def fromZeros ( self ):
+    """Determine if the cube was created from all zeros?"""
+    if self._newcube == True:
+      return True
+    else: 
+      return False
+
+  # create an all zeros cube
+  def zeros ( self ):
+    """Create a cube of all 0"""
+    self._newcube = True
+    self.data = np.zeros ( self.cubesize, dtype=np.uint64 )
+
+  def overwrite ( self, annodata ):
+    """Get's a dense voxel region and overwrites all non-zero values"""
+
+    vector_func = np.vectorize ( lambda a,b: b if b!=0 else a ) 
+    self.data = vector_func ( self.data, annodata ) 
+
+
+  #
+  # Create the specified slice (index) at filename
+  #
+  def xySlice ( self, fileobj ):
+
+    zdim,ydim,xdim = self.data.shape
+    outimage = Image.fromarray( self.data[0,:,:], "RGBA")
+    outimage.save ( fileobj, "PNG" )
+
+
+  #
+  # Create the specified slice (index) at filename
+  #
+  def xzSlice ( self, zscale, fileobj  ):
+
+    zdim,ydim,xdim = self.data.shape
+    outimage = Image.fromarray( self.data[:,0,:], "RGBA")
+    outimage.save ( fileobj, "PNG" )
+
+
+  #
+  # Create the specified slice (index) at filename
+  #
+  def yzSlice ( self, zscale, fileobj  ):
+
+    zdim,ydim,xdim = self.data.shape
+    outimage = Image.fromarray( self.data[:,:,0], "RGBA")
+    outimage.save ( fileobj, "PNG" )
+
+# end BrainCube
