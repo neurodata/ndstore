f757149ff7a836e3ca24e4430b696b7a31a315c5
Kunal Lillaney <lillaney@jhu.edu>
2015-02-18 20:03:39 -0500

    [core] Bug Fixes for merge Code. There is an issue with mergectype code which needs to be fixed.

--- ocpca/annindex.py
+++ ocpca/annindex.py
@@ -108,12 +108,13 @@ class AnnotateIndex:
         self.putIndex ( key, resolution, newIndex, True )
 
   #
-  #  deleteIndex - Delete the index for a given annotation id for current resolution
+  #  deleteIndexResolution - Delete the index for a given annotation id for current resolution
   #
-  #def deleteIndexResolution ( self, annid, resolution ):
-  #  """delete the index for a given annid"""
+  def deleteIndexResolution ( self, annid, res ):
+    """delete the index for a given annid at the given resolution"""
     
     # delete Index table for each resolution
+    self.kvio.deleteIndex(annid,res)
   #  sql = "DELETE FROM " +  self.proj.getIdxTable(resolution)  +  " WHERE annid=" + str(annid)
   #  try:
   #    self.cursor.execute ( sql )
