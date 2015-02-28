a4b4d1e7d0a1c11cc221675cf2266c71d42acaae
Randal Burns <randal@cs.jhu.edu>
2015-02-27 19:05:23 -0500

    Reorder table.

--- django/OCP/wsgi.py
+++ django/OCP/wsgi.py
@@ -40,6 +40,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OCP.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
