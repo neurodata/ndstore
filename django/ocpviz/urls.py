7d2db3055d7d766c22194e49b9eab25cc7c0607e
Alex Baden <alex.baden@gmail.com>
2015-05-21 11:20:13 -0400

    [viz]: fixed bug in urls.py that broke queries

--- django/ocpviz/urls.py
+++ django/ocpviz/urls.py
@@ -18,9 +18,9 @@ import django.contrib.auth
 
 urlpatterns = patterns('ocpviz.views',
     url(r'^project/(?P<webargs>[\w,/-]+)', 'projectview'),
-    url(r'(?P<webargs>[\w,/-]+)$', 'tokenview'),
     # for redirecting queries (reqd because of same origin policy)
     url(r'^query/(?P<queryargs>[\w,/-]+)', 'query'),
+    url(r'(?P<webargs>[\w,/-]+)$', 'tokenview'),
     #'(?P<queryargs>^\w+)$', 'query'),
     url(r'^$', 'default'),
     url(r'^manage/$', 'default'),
