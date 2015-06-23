f568cfe6c909705f19828801109c789e1b2518cc
Alex Eusman <Aeus@jhu.edu>
2015-06-23 15:12:13 -0400

    microcommit

--- setup/SetupScripts/installscriptUbuntu.sh
+++ setup/SetupScripts/installscriptUbuntu.sh
@@ -2,13 +2,15 @@
 echo -n "Install script should be placed in the open-connectome folder. "
 sudo apt-get install Python-pip mysql-server python-mysqldb libmysqld-dev python-dev liblapack-dev gfortran libmemcached-dev Libhdf5-dev python-pytest
 
-echo -n "Do you wish to create a virtual enviroment (Named OCPServer) (y/n)?" 
+echo -n "Do you wish to create a virtual enviroment (Named OCPServer) in the /home directory (y/n)? please edit the location if you wish it to be in the /home/user directory" 
 read answerenv
 
 if echo "$answerenv" | grep -iq "^y" ;then
 	sudo apt-get install python-virtualenv
+#CAN EDIT	
 	sudo virtualenv /home/OCPServer
 	source OCPServer/bin/activate
+#DO NOT EDIT
 else
     	echo -n "Please note it is easier to install django and other packages in a virtual enviroment."
 fi
@@ -17,23 +19,8 @@ fi
 echo -n "Do you wish to install the python packages through pip (y/n)?" 
 read answer
 if echo "$answer" | grep -iq "^y" ;then
-    	pip install numpy
-	pip install Scipy
-	pip install ez_setup
-	pip install Fipy
-	pip install Django
-	pip install Django-registration
-	pip install Django-celery
-	pip install MySQL-python
-	pip install TurboGears
-	pip install Django-registration-redux
-	pip install Cython
-	pip install H5py
-	pip install Pillow
-	pip install Cheetah
-	pip install Registration
-	pip install Pylibmc
-	pip install uWSGI
+	source OCPServer/bin/activate
+    	pip install numpy Scipy ez_setup Fipy Django Django-registration Django-celery MySQL-python TurboGears Django-registration-redux Cython H5py Pillow Cheetah Registration Pylibmc uWSGI
 
 else
     	echo -n "Please add the packages later manually."
