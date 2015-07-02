b6afa3fe111cb49eeeb9b1195f023ca96a86cdfc
aeus <aeus@jhu.edu>
2015-07-02 16:26:13 -0400

    Process fully automated except editing settings.y and secret_settings.py values

--- setup/AutoSetup/installscriptUbuntu.sh
+++ setup/AutoSetup/installscriptUbuntu.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+if [ $EUID != 0 ]; then
+	sudo "$0" "$@"
+	exit $?
+fi
+
 # Install basic pacakges
 echo -n "Install script should be placed in the open-connectome folder. "
 
 MySQLPass='your_password'
 BrainPass=''
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '$MySQLPass
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '$MySQLPass
-sudo apt-get -y install mysql-server
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password '$MySQLPass
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '$MySQLPass
+apt-get -y install mysql-server
 
-sudo apt-get -y install Python-pip python-mysqldb libmysqld-dev python-dev liblapack-dev gfortran libmemcached-dev Libhdf5-dev python-pytest python-virtualenv
+apt-get -y install Python-pip python-mysqldb libmysqld-dev python-dev liblapack-dev gfortran libmemcached-dev Libhdf5-dev python-pytest python-virtualenv
 
 # Setup and install packages to the virtual enviroment
 virtualenv ../OCPServer
@@ -26,10 +31,10 @@ pip install Django-registration-redux Cython H5py Pillow Cheetah Registration Py
 cp django/OCP/settings_secret.py.example django/OCP/settings_secret.py
 cp django/OCP/settings.py.example django/OCP/settings.py
 
-sudo mkdir /var/log/ocp
-sudo chown www-data:www-data /var/log/ocp
-sudo touch /var/log/ocp/ocp.log
-sudo chmod 777 /var/log/ocp/ocp.log
+mkdir /var/log/ocp
+chown www-data:www-data /var/log/ocp
+touch /var/log/ocp/ocp.log
+chmod 777 /var/log/ocp/ocp.log
 
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
