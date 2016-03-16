f1a9172ab3b06b711eef6408d72ea8d5de200a35
randal@cs.jhu.edu <Randal Burns>
2016-03-16 01:40:05 -0600

    Synaptogram service simplified and will return json file for rendering on front end.

--- django/synaptogram/views.py
+++ django/synaptogram/views.py
@@ -13,18 +13,17 @@
 # limitations under the License.
 
 # Create your views here.
-import django.http
-
 import numpy as np
-from PIL import Image
-import urllib2
-import zlib
-import cStringIO
 import re
+from contextlib import closing
+import cStringIO
+import django.http
 
 import ndproj
 import ndwsrest
-import synaptogram
+import spatialdb
+
+import json
 
 
 """RESTful interface to a synaptogram.  Rerturn a png file."""
@@ -34,70 +33,52 @@ def synaptogram_view (request, webargs):
 
   try:
 
-    # token and channels
-    token, chanstr, centroidstr, rest = webargs.split('/',3)
+    m = re.match ("(?P<token>[\d\w]+)/(?P<channels>[\d\w,]+)/(xy|xz|yz)/(?P<resolution>[\d]+)/(?P<xlow>[\d]+),(?P<xhigh>[\d]+)/(?P<ylow>[\d]+),(?P<yhigh>[\d]+)/(?P<zlow>[\d]+),(?P<zhigh>[\d]+)/", webargs)
+    md = m.groupdict()
+
+    token = md['token']
+    chanstr = md['channels']
+    resolution = int(md['resolution'])
+    xlow = int(md['xlow'])
+    xhigh = int(md['xhigh'])
+    ylow = int(md['ylow'])
+    yhigh = int(md['yhigh'])
+    zlow = int(md['zlow'])
+    zhigh = int(md['zhigh'])
 
     channels = chanstr.split(',')
-    centroid =  map(lambda x: int(x),centroidstr.split(','))
+
+    # pattern for using contexts to close databases
+    # get the project 
+    with closing ( ndproj.NDProjectsDB() ) as projdb:
+      proj = projdb.loadToken ( token )
+
+    # and the database and then call the db function
+    with closing ( spatialdb.SpatialDB(proj) ) as db:
+
+      # convert to cutout coordinates
+      corner = [ xlow, ylow, zlow ]
+      dim = [ xhigh-xlow, yhigh-ylow, zhigh-zlow ]
+
+      outputdict = {}
+
+      # get the data region for each channel 
+      for chan in channels:
+        # data type on a per channel basis
+        ch = proj.getChannelObj(chan)
+        try: 
+          cb = db.cutout ( ch, corner, dim, resolution )
+          outputdict[chan] = cb.data.tolist()
+          outputdict['{}.dtype'.format(chan)] = str(cb.data.dtype)
+        except KeyError:
+          raise Exception ("Channel %s not found" % ( chan ))
+
+      outputdict['shape'] = cb.data.shape
+
+      jsonstr = json.dumps ( outputdict )
+
+      return django.http.HttpResponse(json.dumps(outputdict), content_type="application/json")
 
 
-    # create the synaptogram object
-    sog = synaptogram.Synaptogram ( token, channels, centroid )
-
-    # set options
-    # if there is a reference string in the URL
-    s = re.search ( 'reference/([\w\-,]+)/', rest )
-    if s != None:
-      sog.setReference(s.group(1).split(','))
-
-    # tell which channels are EM channels -- no reference
-    s = re.search ( 'EM/([\w\-,]+)/', rest )
-    if s != None:
-      sog.setEM(s.group(1).split(','))
-
-    # if there is a enhance string in the URL
-    s = re.search ( 'enhance/([\d.]+)/', rest )
-    if s != None:
-      sog.setEnhance(float(s.group(1)))
-      
-    s = re.search ( 'normalize/', rest )
-    if s != None:
-      sog.setNormalize()
-
-    s = re.search ( 'normalize2/', rest )
-    if s != None:
-      sog.setNormalize2()
-
-    # resolution
-    s = re.search ( 'resolution/([\d]+)/', rest )
-    if s != None:
-      sog.setResolution(int(s.group(1)))
-
-    # Width of the input data
-    s = re.search ( 'width/([\d]+)/', rest )
-    if s != None:
-      sog.setWidth(int(s.group(1)))
-
-    # Width of the input data
-    s = re.search ( 'tile/([\d]+)/', rest )
-    if s != None:
-      sog.setTileWidth(int(s.group(1)))
-
-    # Width of the input data
-    s = re.search ( 'frame/([\d]+)/', rest )
-    if s != None:
-      sog.setFrameWidth(int(s.group(1)))
-
-    # construct the picture
-    sogimg = sog.construct()
-
-    # Draw the image file
-    fobj = cStringIO.StringIO() 
-    sogimg.save ( fobj, "PNG" )
-    fobj.seek(0)
-    return django.http.HttpResponse(fobj.read(), mimetype="image/png" )
-
-  except Exception, e:
+  except:
     raise
-#    return django.http.HttpResponseNotFound(e)
-
