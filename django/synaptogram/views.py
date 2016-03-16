e62cb29e76f1850771b9aa5cfbed2c1f94b340a1
Ubuntu <ubuntu@ip-172-31-27-105.us-west-2.compute.internal>
2016-03-16 18:19:26 +0000

    Reinstate new synaptogram.

--- django/synaptogram/views.py
+++ django/synaptogram/views.py
@@ -25,6 +25,7 @@ import spatialdb
 
 import json
 
+#import synaptogram
 
 """RESTful interface to a synaptogram.  Rerturn a png file."""
 
@@ -82,3 +83,76 @@ def synaptogram_view (request, webargs):
 
   except:
     raise
+
+def synaptogram_view_old (request, webargs):
+  """Render a synaptogram as a Web page"""
+
+  try:
+
+    # token and channels
+    token, chanstr, centroidstr, rest = webargs.split('/',3)
+
+    channels = chanstr.split(',')
+    centroid =  map(lambda x: int(x),centroidstr.split(','))
+
+
+    # create the synaptogram object
+    sog = synaptogram.Synaptogram ( token, channels, centroid )
+
+    # set options
+    # if there is a reference string in the URL
+    s = re.search ( 'reference/([\w\-,]+)/', rest )
+    if s != None:
+      sog.setReference(s.group(1).split(','))
+
+    # tell which channels are EM channels -- no reference
+    s = re.search ( 'EM/([\w\-,]+)/', rest )
+    if s != None:
+      sog.setEM(s.group(1).split(','))
+
+    # if there is a enhance string in the URL
+    s = re.search ( 'enhance/([\d.]+)/', rest )
+    if s != None:
+      sog.setEnhance(float(s.group(1)))
+      
+    s = re.search ( 'normalize/', rest )
+    if s != None:
+      sog.setNormalize()
+
+    s = re.search ( 'normalize2/', rest )
+    if s != None:
+      sog.setNormalize2()
+
+    # resolution
+    s = re.search ( 'resolution/([\d]+)/', rest )
+    if s != None:
+      sog.setResolution(int(s.group(1)))
+
+    # Width of the input data
+    s = re.search ( 'width/([\d]+)/', rest )
+    if s != None:
+      sog.setWidth(int(s.group(1)))
+
+    # Width of the input data
+    s = re.search ( 'tile/([\d]+)/', rest )
+    if s != None:
+      sog.setTileWidth(int(s.group(1)))
+
+    # Width of the input data
+    s = re.search ( 'frame/([\d]+)/', rest )
+    if s != None:
+      sog.setFrameWidth(int(s.group(1)))
+
+    # construct the picture
+    sogimg = sog.construct()
+
+    # Draw the image file
+    fobj = cStringIO.StringIO() 
+    sogimg.save ( fobj, "PNG" )
+    fobj.seek(0)
+    return django.http.HttpResponse(fobj.read(), content_type="image/png" )
+
+  except Exception, e:
+    raise
+#    return django.http.HttpResponseNotFound(e)
+
