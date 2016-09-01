cc0b8fb40b90a5718bdd24abf3bdddb6bc23f1ca
Alex Eusman <alex.eusman@gmail.com>
2016-09-01 15:20:09 -0400

    New Permission Class

---
+++ django/ndauth/authentication.py
@@ -0,0 +1,61 @@
+# myapp/authentication.py
+from django.contrib.auth.models import User
+
+from rest_framework import authentication
+from rest_framework import exceptions
+from nduser.models import Token
+
+from django.contrib.auth import authenticate
+#from rest_framework.authtoken.models import Token
+from django.http import HttpResponseForbidden
+
+from ndwserror import NDWSError
+import logging
+logger=logging.getLogger("neurodata")
+
+class PublicAuthentication(authentication.BaseAuthentication):
+  def has_permission(self, request, view):
+    web_path = request.path
+    if not web_path: # Some very odd scenario
+      raise IOError('Could not get web_path from reques')
+
+    try:
+      args = web_path.split('/')
+      # Expects args in order: ''|nd|ocp , ''|nd|ocp , App , View , Token , Arguements
+      if args[0] in ['','nd','ocp']:
+        args = args[1:]
+      if args[0] in ['','nd','ocp']:
+        args = args[1:]
+
+      if args[0] == 'catmaid':
+        exp_token = args[2]
+      else:
+        exp_token = args[1]
+
+      pp_tokens = Token.objects.filter(public=1)
+      pub_tokens = []
+      for v in pp_tokens.values():
+        pub_tokens.append(v['token_name'])
+
+      if exp_token in pub_tokens and request.method == 'GET':
+        return True
+
+      try:
+        if not request.user.is_superuser:
+          m_tokens = Token.objects.filter(user=request.user.id)
+          tokens = []
+          for v in m_tokens.values():
+            tokens.append(v['token_name'])
+          if exp_token not in tokens:
+            raise NDWSError ("Token {} does not exist or you do not have\
+                              sufficient permissions to access it. {}".format(exp_token, pub_tokens))
+        else:
+          return True
+      except Exception as e:
+        logger.warning("Error in authenticating user in NDAUTH: \
+{}".format(e))
+        return False
+
+    except Exception as e:
+      raise e
+
