7665b71678d259c232678ffcfe7c06f088fdc843
Kunal Lillaney <lillaney@jhu.edu>
2016-10-07 17:36:49 -0400

    [resource] Adding new django app to edit resources,

---
+++ django/resource/urls.py
@@ -0,0 +1,26 @@
+# Copyright 2014 NeuroData (http://neurodata.io)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.conf.urls import url
+from datasetview import DatasetView
+from projectview import ProjectView
+from channelview import ChannelView
+from tokenview import TokenView
+
+urlpatterns = [
+  url(r'dataset/(?P<dataset_name>[\w_-]+)/?$', DatasetView.as_view()),
+  url(r'dataset/(?P<dataset_name>[\w_-]+)/project/(?P<project_name>[\w_-]+)/?$', ProjectView.as_view()),
+  url(r'dataset/(?P<dataset_name>[\w_-]+)/project/(?P<project_name>[\w_-]+)/channel/(?P<channel_name>[\w_-]+)/?$', ChannelView.as_view()),
+  url(r'dataset/(?P<dataset_name>[\w_-]+)/project/(?P<project_name>[\w_-]+)/token/(?P<token_name>[\w_-]+)/?$', TokenView.as_view()),
+]
