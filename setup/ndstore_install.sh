c3fc48e2caa5ea1fd4c5f63cc85da95a36a3f37c
Alex Eusman <Alex.Eusman@gmail.com>
2016-12-28 13:54:31 -0500

    Mysql pre-installed on travis for some reason, removing password preset normally

--- setup/ndstore_install.sh
+++ setup/ndstore_install.sh
@@ -63,7 +63,7 @@ sudo -u neurodata make -f makefile_LINUX
 # configure mysql
 cd /home/neurodata/ndstore/django/
 sudo service mysql start
-mysql -u root -pneur0data -i -e "create user 'neurodata'@'localhost' identified by 'neur0data';" && mysql -u root -pneur0data -i -e "grant all privileges on *.* to 'neurodata'@'localhost' with grant option;" && mysql -u neurodata -pneur0data -i -e "CREATE DATABASE neurodjango;"
+mysql -u root -p -i -e "create user 'neurodata'@'localhost' identified by 'neur0data';" && mysql -u root -p -i -e "grant all privileges on *.* to 'neurodata'@'localhost' with grant option;" && mysql -u neurodata -pneur0data -i -e "CREATE DATABASE neurodjango;"
 
 # configure django setttings
 cd /home/neurodata/ndstore/django/ND/
