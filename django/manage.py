1b2f350cdaf66c83fc7e53ed28fba070da966f26
randal@cs.jhu.edu <Randal Burns>
2017-02-08 16:59:56 -0500

    In flight to python 3

--- django/manage.py
+++ django/manage.py
@@ -18,6 +18,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ND.settings")
 
     from django.core.management import execute_from_command_line
