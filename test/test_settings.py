0f0e3f42e1964b776cd1b78b58261be30daea437
Kunal Lillaney <lillaney@jhu.edu>
2017-02-27 16:42:15 -0500

    Modifications for new timeeries format.

--- test/test_settings.py
+++ test/test_settings.py
@@ -17,7 +17,7 @@ import sys
 sys.path += [os.path.abspath('../django')]
 import ND.settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'ND.settings'
-from ndlib.ndtype import MYSQL, CASSANDRA, RIAK, DYNAMODB, REDIS
+from ndlib.ndtype import *
 
 DEV_MODE = True
 
@@ -26,11 +26,11 @@ KV_ENGINE = MYSQL
 # kvengine = CASSANDRA
 # kvengine = RIAK
 # kvengine = DYNAMODB
-# kvengine = REDIS
+# KV_ENGINE = REDIS
 
 # kvserver settings
 KV_SERVER = 'localhost'
 
 # server to check against
-SITE_HOST = 'localhost:8000'
-#SITE_HOST = 'localhost/nd'
+# SITE_HOST = 'localhost:8080'
+SITE_HOST = 'localhost/nd'
